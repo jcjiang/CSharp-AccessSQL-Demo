6382f753ebca514c2100511b3bdbe1f75800c2f6
Jiachen Jiang <jiachen.jiang@microsoft.com>
2023-03-17 14:53:26 -0700

    Added ADO.NET and Dapper

---
+++ SQLDatabaseAccesser/ADONET/ADOSQLAccess.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLDatabaseAccesser.ADONET
+{
+    internal class ADOSQLAccess
+    {
+        public List<Blog> GetBlog(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database=master;Trusted_Connection=True;"))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM BlogId";
+
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine(dataReader["Url"].ToString());    
+                    }
+                    dataReader.Close();
+                }
+            }
+        }
+    }
+}
