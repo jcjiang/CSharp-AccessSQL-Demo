c627777bffd086c96c66a21437669255c2453aad
Jiachen Jiang <jiachen.jiang@microsoft.com>
2023-03-21 11:06:29 -0700

    Changed code structure to three separate projects

---
+++ ADOAccess/ADOSQLAccess.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLDatabaseAccesser.ADOAccess;
+internal class ADOSQLAccess
+{
+    public void GetBlog(int id)
+    {
+        using SqlConnection conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database=master;Trusted_Connection=True;");
+        
+        conn.Open();
+        using SqlCommand cmd = conn.CreateCommand();
+        
+        cmd.Connection = conn;
+        cmd.CommandText = "SELECT * FROM BlogId";
+
+        using SqlDataReader dataReader = cmd.ExecuteReader();
+
+        while (dataReader.Read())
+        {
+            Console.WriteLine(dataReader["Url"].ToString());    
+        }
+    }
+}
---
+++ ADOAccess/Program.cs
@@ -0,0 +1,3 @@
+﻿using System;
+
+Console.WriteLine("Querying for a blog");
